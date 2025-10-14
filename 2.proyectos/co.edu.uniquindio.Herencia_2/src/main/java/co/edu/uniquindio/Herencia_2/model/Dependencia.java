fa7ab09fe6db3cfacdaa9644e1fb4bdeb3881d2a
Sebastian Garcia <jssolorzanog@uqvirtual.edu.co>
2025-10-13 21:13:35 -0500

    Herencia de clase Docente. Implementacion del CURD completado. Aun no se toma encuenta las clases hijas en el CRUD

---
+++ 2.proyectos/co.edu.uniquindio.Herencia_2/src/main/java/co/edu/uniquindio/Herencia_2/model/Dependencia.java
@@ -0,0 +1,85 @@
+package co.edu.uniquindio.Herencia_2.model;
+
+import javax.print.Doc;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Dependencia {
+    private ArrayList<Docente> listaDocentes = new ArrayList();
+
+    public boolean crearDocente(Docente docente){
+        Docente docenteEncontrado = obternerDocente(docente.getCedula());
+        if(docenteEncontrado == null){
+            Docente docenteNuevo = new Docente();
+            docenteNuevo.setNombre(docente.getNombre());
+            docenteNuevo.setCedula(docente.getCedula());
+
+            getListaDocentes().add(docenteNuevo);
+
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public ArrayList<Docente> listarDocente(){
+        return listaDocentes;
+    }
+    public boolean editarDocente(Docente docente, int nuevaCedula) {
+        Docente docenteEncontrado = obternerDocente(docente.getCedula());
+        if(docenteEncontrado != null) {
+            // Verificar que la nueva cédula no exista (si es diferente)
+            if(docente.getCedula() != nuevaCedula) {
+                Docente docenteConNuevaCedula = obternerDocente(nuevaCedula);
+                if(docenteConNuevaCedula != null) {
+                    return false; // La nueva cédula ya existe
+                }
+            }
+            docenteEncontrado.setNombre(docente.getNombre());
+            docenteEncontrado.setCedula(nuevaCedula);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean borrarDocente(int cedula){
+        Docente docenteBorrar = obternerDocente(cedula);
+        if(docenteBorrar != null){
+            getListaDocentes().remove(docenteBorrar);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public Docente obternerDocente(int cedula){
+        Docente docenteEncontrado = null;
+        for(Docente docente : getListaDocentes()){
+            if(docente.getCedula() == cedula){
+                docenteEncontrado = docente;
+                break;
+            }
+        }
+        return docenteEncontrado;
+    }
+
+
+    public double calcularSalarioDocente(int cantidadPuntos){
+        int valorPunto = 12120;
+        double salarioTotal = cantidadPuntos*valorPunto;
+
+        return salarioTotal;
+    }
+    public double calcularSalarioDocente(Categoria categoria){
+        double salarioTotal = categoria.getSalario();
+
+        return salarioTotal;
+    }
+    public ArrayList<Docente> getListaDocentes() {
+        return listaDocentes;
+    }
+
+    public void setListaDocentes(ArrayList<Docente> listaDocentes) {
+        this.listaDocentes = listaDocentes;
+    }
+
+
+}
